2a33a30e3eb1f965651845850b128aa7af814315
dinh17 <nguyentruongcongdinh@gmail.com>
2022-12-15 17:10:22 +0700

    OK

---
+++ case_study_module_3/database/query_05-10_sql.sql
@@ -0,0 +1,58 @@
+use furama_resort ;
+
+-- 6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, dien_tich, ten_loai_dich_vu của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện 
+--       đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+        select dv.ma_dich_vu,dv.ten_dich_vu,dv.dien_tich,dv.dien_tich,ldv.ten_loai_dich_vu,hd.ngay_lam_hop_dong from dich_vu dv 
+        join loai_dich_vu ldv on ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu 
+        join hop_dong hd on hd.ma_dich_vu = dv.ma_dich_vu 
+        where not(month(hd.ngay_lam_hop_dong) in (1,2,3))
+        group by dv.ma_dich_vu
+        ;
+        -- đáp án so với excel sai 
+
+--  7.	Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ đã từng 
+--      được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
+select dv.ma_dich_vu, dv.ten_dich_vu,dv.dien_tich,dv.so_nguoi_toi_da,dv.chi_phi_thue,ldv.ten_loai_dich_vu , hd.ngay_lam_hop_dong
+from hop_dong hd
+join  dich_vu dv on hd.ma_dich_vu = dv.ma_dich_vu
+join  loai_dich_vu ldv on ldv.ma_loai_dich_vu = dv.ma_loai_dich_vu
+where year( hd.ngay_lam_hop_dong) = '2020' and not(year(hd.ngay_lam_hop_dong)='2021');
+
+ -- đáp án so với excel sai 
+
+-- 8.	Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+--      Học viên sử dụng theo 3 cách khác nhau để thực hiện yêu cầu trên.
+
+-- cách 1 
+ select ho_ten from khach_hang
+ group by (ho_ten);
+ -- cách 2 
+ select distinct ho_ten from Khach_hang;
+ -- cách 3 
+ select kh.ho_ten
+from khach_hang as kh
+union
+select kh.ho_ten
+from khach_hang as kh;
+ 
+-- 9.	Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
+select 
+  month(hd.ngay_lam_hop_dong ) as thang, 
+  count(hd.ma_khach_hang) as so_luong_khach_hang ,
+  hd.ma_khach_hang
+from 
+  hop_dong hd 
+group by 
+  thang
+order by 
+  thang ;
+
+
+
+
+-- 10.	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong,
+--      ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
+
+select 
+
+
---
+++ ss2_design_and_create_db/exercise/change_ERD.sql
@@ -0,0 +1,64 @@
+create database material_management;
+use material_management;
+-- tao bang phieu xuat
+create table phieu_xuat (
+  so_px int primary key, ngay_xuat datetime
+);
+-- tao bang vat tu 
+create table vat_tu (
+  ma_vat_tu int primary key, 
+  ten_vat_tu varchar(100)
+);
+-- tao bang chi_tiet_phieu_xuat 
+create table chi_tiet_phieu_xuat (
+  so_px int, 
+  ma_vat_tu int, 
+  primary key (so_px, ma_vat_tu), 
+  don_gia_xuat double not null, 
+  so_luong_xuat bigint, 
+  foreign key(so_px) references phieu_xuat(so_px), 
+  foreign key(ma_vat_tu) references vat_tu(ma_vat_tu)
+);
+-- tao bang phieu_nhap 
+create table phieu_nhap(
+  so_phieu_nhap int primary key, ngay_nhap date
+);
+-- tao bang chi tiet phieu nhap 
+create table chi_tiet_phieu_nhap (
+  ma_vat_tu int, 
+  so_phieu_nhap int, 
+  don_gia_nhap double, 
+  so_luong_nhap int, 
+  primary key(ma_vat_tu, so_phieu_nhap), 
+  foreign key(ma_vat_tu) references vat_tu(ma_vat_tu), 
+  foreign key(so_phieu_nhap) references phieu_nhap(so_phieu_nhap)
+);
+-- tao bang don_dat_hang
+create table don_dat_hang (
+  so_don_hang int primary key, 
+  ngay_don_hang date, 
+  ma_nha_cung_cap int, 
+  foreign key(ma_nha_cung_cap) references nha_cung_cap(ma_nha_cung_cap)
+);
+-- tao bang chi_tiet_don_dat_hang 
+create table chi_tiet_don_dat_hang (
+  ma_vat_tu int, 
+  so_don_hang int, 
+  primary key(ma_vat_tu, so_don_hang), 
+  foreign key(ma_vat_tu) references vat_tu(ma_vat_tu), 
+  foreign key(so_don_hang) references don_dat_hang(so_don_hang)
+);
+--  tao bang nha_cung_cap
+create table nha_cung_cap (
+  ma_nha_cung_cap int primary key, 
+  ten_nha_cung_cap varchar(120), 
+  dia_chi varchar(120)
+
+);
+
+create table so_dien_thoai (
+id int primary key ,
+ma_nha_cung_cap int ,
+foreign key (ma_nha_cung_cap) references nha_cung_cap(ma_nha_cung_cap)
+);
+
