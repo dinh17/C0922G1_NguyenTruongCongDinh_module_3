be687347cd13473b37c31973072901c7213b37e6
dinh17 <nguyentruongcongdinh@gmail.com>
2022-12-16 08:08:45 +0700

    OK

--- case_study_module_3/database/query_02_5_sql.sql
+++ case_study_module_3/database/query_02_5_sql.sql
@@ -24,17 +24,27 @@ order by
 --      tong_tien (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet)
 --      cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
 select 
-kh.ma_khach_hang,
- kh.ho_ten, 
- lk.ten_loai_khach,
- hd.ma_hop_dong,
- dv.ten_dich_vu,
- hd.ngay_lam_hop_dong,
- hd.ngay_ket_thuc,
+  kh.ma_khach_hang, 
+  kh.ho_ten, 
+  lk.ten_loai_khach, 
+  hd.ma_hop_dong, 
+  dv.ten_dich_vu, 
+  hd.ngay_lam_hop_dong, 
+  hd.ngay_ket_thuc, 
   (
     ifnull(dv.chi_phi_thue, 0) + sum(
       ifnull(dvdk.gia, 0) * ifnull(hdct.so_luong, 0)
     )
   ) as Tong_tien 
-  from khach_hang kh 
-  join loai_khach lk on lk.ma_loai_khach
+from 
+  loai_khach lk 
+  right join khach_hang kh on lk.ma_loai_khach = kh.ma_loai_khach 
+  left join hop_dong hd on hd.ma_khach_hang = kh.ma_khach_hang 
+  left join dich_vu dv on dv.ma_dich_vu = hd.ma_dich_vu 
+  left join hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma_hop_dong 
+  left join dich_vu_di_kem dvdk on dvdk.ma_dich_vu_di_kem = hdct.ma_dich_vu_di_kem 
+group by 
+  hd.ma_hop_dong, 
+  kh.ma_khach_hang
+  order by 
+  ma_khach_hang ;
--- case_study_module_3/database/query_05-10_sql.sql
+++ case_study_module_3/database/query_05-10_sql.sql
@@ -47,12 +47,16 @@ group by
 order by 
   thang ;
 
-
-
-
 -- 10.	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong,
 --      ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
+select hd.ma_hop_dong , hd.ngay_lam_hop_dong,hd.ngay_ket_thuc,hd.tien_dat_coc, 
+ifnull(
+    sum(hdct.so_luong), 
+    0
+  ) as so_luong 
+from hop_dong hd 
+ left join hop_dong_chi_tiet hdct on hdct.ma_hop_dong = hd.ma_hop_dong
+ group by hd.ma_hop_dong ;
 
-select 
 
 
